Fixed bug in the computation of the Jacobian.

src/examples/train_model.py
@@ -31,13 +31,14 @@ trainer = Trainer(net, pos, vel)
 
 # Set trainer optimizer (this is not very clean)
 trainer.optimizer = torch.optim.Adam(
-    trainer.model.parameters(), lr=1e-4,  weight_decay=1e-8)
+    trainer.model.parameters(), lr=1e-4,  weight_decay=1e-10)
 
 # Set trainer loss
 trainer.loss = torch.nn.SmoothL1Loss()
 
 # Set trainer options
-trainer.options(normalize=False, shuffle=True, print_loss=True, epochs=100)
+trainer.options(normalize=False, shuffle=True,
+                print_loss=True, epochs=500, load_model=False)
 
 # Train model
 trainer.train()

src/learn_diffeomorphism/diffeomorphism.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 
+import torch
 import torch.nn as nn
 from .coupling_layer import CouplingLayer
-from time import time
 
 
 class Diffeomorphism(nn.Module):
     def __init__(self, dim, num_features, num_diff, length=1):
         super(Diffeomorphism, self).__init__()
 
+        self.dim_ = dim
+
         self.prediction_ = nn.Sequential(*(CouplingLayer(dim, num_features, i % 2, length)
                                            for i in range(num_diff)))
 
     def forward(self, x):
         return self.prediction_(x)
+
+    def jacobian(self, x):
+        y = self.prediction_(x)
+
+        jac = torch.empty(x.size(0)*self.dim_, self.dim_)
+
+        for i in range(self.dim_):
+            grad = torch.autograd.grad(
+                y[:, i], x, grad_outputs=torch.ones_like(y[:, i]), create_graph=True)[0]
+            jac[:, i] = grad.reshape(1, -1)
+
+        return jac.reshape(-1, self.dim_, self.dim_).permute(0, 2, 1), y

src/learn_diffeomorphism/dynamics.py
@@ -4,13 +4,6 @@ import torch
 import torch.nn as nn
 
 from .diffeomorphism import Diffeomorphism
-from src.learn_diffeomorphism.utils import blk_matrix
-
-from time import time
-
-
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
 
 
 class Dynamics(nn.Module):
@@ -25,24 +18,17 @@ class Dynamics(nn.Module):
             dim, num_features, num_diff,  length)
 
     def forward(self, x):
-        m = x.size(0)
-        y = self.diffeomorphism_(x)
-
+        # Calculate attactor location in the linear space
         attractor = self.diffeomorphism_(self.attractor_)[0]
 
-        jac = torch.empty(m*self.dim_, self.dim_).to(device)
+        # Calculate diffeomorphism and jacobian
+        J, y = self.diffeomorphism_.jacobian(x)
 
-        for i in range(self.dim_):
-            grad = torch.autograd.grad(
-                y[:, i], x, grad_outputs=torch.ones_like(y[:, i]), retain_graph=True)[0]
-            jac[:, i] = grad.reshape(1, -1)
+        # Calculate minus gradient of the potential function in linear space
+        _, dy = self.potential(attractor - y)
 
-        jac = jac.reshape(m, self.dim_, self.dim_).permute(0, 2, 1)
+        # Return DS in the original space
+        return torch.bmm(torch.inverse(J), dy.unsqueeze(2)).squeeze()
 
-        result = torch.empty(m, self.dim_).to(device)
-
-        for i in range(m):
-            result[i, :] = -torch.mv(torch.inverse(torch.mm(jac[i].transpose(
-                1, 0), jac[i])), y[i, :]-attractor)
-
-        return result
+    def potential(self, y):
+        return torch.bmm(y.unsqueeze(1), y.unsqueeze(2)), y

src/learn_diffeomorphism/trainer.py
@@ -4,7 +4,7 @@ import os
 
 class Trainer:
     __options__ = ['epochs', 'batch', 'normalize',
-                   'shuffle', 'print_epoch', 'print_loss', 'load_model']
+                   'shuffle', 'print_loss', 'load_model', 'clip_grad']
 
     def __init__(self, model, input, target):
         # Set the model
@@ -29,7 +29,6 @@ class Trainer:
             batch=None,
             normalize=False,
             shuffle=True,
-            print_epoch=False,
             print_loss=True,
             load_model=False,
             clip_grad=None)
@@ -68,10 +67,7 @@ class Trainer:
 
         # start training
         for epoch in range(self.options_['epochs']):
-            if self.options_["print_epoch"]:
-                print("EPOCH: ", epoch)
-
-            for _, (batch_x, batch_y) in enumerate(loader):  # for each training step
+            for iter, (batch_x, batch_y) in enumerate(loader):  # for each training step
                 b_x = torch.autograd.Variable(batch_x, requires_grad=True)
                 b_y = torch.autograd.Variable(batch_y)
 
@@ -82,7 +78,8 @@ class Trainer:
                 loss = self.loss(prediction, b_y)
 
                 if self.options_["print_loss"]:
-                    print(loss)
+                    print("EPOCH: ", epoch, "ITER: ",
+                          iter, "LOSS: ", loss.item())
 
                 # clear gradients for next train
                 self.optimizer.zero_grad()
@@ -91,9 +88,9 @@ class Trainer:
                 loss.backward()
 
                 # Clip grad if requested
-                if self.options_["clip_gradient"] is not None:
+                if self.options_["clip_grad"] is not None:
                     torch.nn.utils.clip_grad_norm_(
-                        self.model.parameters(), self.options_["clip_gradient"])
+                        self.model.parameters(), self.options_["clip_grad"])
 
                 # apply gradients
                 self.optimizer.step()